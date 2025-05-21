6620f6a23a430ebd7c00508b123007e1103407a5
Fabian Molina <famole@gmail.com>
2025-05-21 20:28:28 -0300

    chore(auth): fix AuthContextType import

--- src/context/auth/useAuthProvider.tsx
+++ src/context/auth/useAuthProvider.tsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react';
 import { supabase } from '@/integrations/supabase/client';
-import { AuthContextType } from './AuthProvider';
+import { AuthContextType } from '@/types/auth';
 import { User } from '@/types';
 import { 
   cleanupAuthState, 
