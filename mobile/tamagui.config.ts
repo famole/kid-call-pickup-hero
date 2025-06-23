475c060e2c98609439f46afe059e6aa0066e2f99
Fabian Molina <famole@gmail.com>
2025-06-23 19:28:41 -0300

    .tamagui

--- mobile/tamagui.config.ts
+++ mobile/tamagui.config.ts
@@ -1,6 +1,11 @@
 import { config } from '@tamagui/config/v3'
 import { createTamagui } from 'tamagui'
 
-// Re-export the default Tamagui config. This keeps things simple while
-// providing a polished set of components and themes.
-export default createTamagui(config)
+const tamaguiConfig = createTamagui(config)
+
+export type Conf = typeof tamaguiConfig
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+}
+
+export default tamaguiConfig
